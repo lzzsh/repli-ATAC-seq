ccb700df9bec76134a8ef64b8cb0b015159a31fc
lzzsh <liaozizhuo@westlake.edu.cn>
2024-10-16 21:57:27 +0800

    Create Figure2B.R

---
+++ Figure2B.R
@@ -0,0 +1,39 @@
+library(dplyr)
+library(ggplot2)
+histone <- read.table("~/Desktop/R所需文件/peaks划分/modify_RT.bed")
+colnames(histone)[1:3]<- c("chr","start","end")
+
+histone <- merge(histone,peaks_reads[,c("chr","start","end","RT")],by=c("chr","start","end")) 
+histone <- histone %>%
+  select(4,5,6,7,9)%>%
+  filter(!(RT %in% c("EL","EML")))%>%
+  setNames(c("chr","start","end","Modify","RT")) %>%
+  arrange(across(everything()))
+
+RT_freq <- peaks_reads %>% group_by(RT)%>%
+  summarise(Sum = sum(end) - sum(start))%>%
+  filter(!(RT %in% c("EL","EML")))%>%
+  mutate(percent = Sum/sum(Sum), RT = factor(RT,levels = c("E", "EM", "M","ML","L")))
+
+histone_freq <- as.data.frame(table(histone$Modify,histone$RT)) %>%
+  setNames(c("Modify","RT","Freq")) %>%
+  group_by(Modify) %>%
+  reframe(RT = RT, percent = Freq / sum(Freq))
+
+modify_plot <- merge(histone_freq, RT_freq, by = "RT") %>%
+  select(1,2,3,5) %>%
+  mutate(percent.fix = percent.x / percent.y)
+
+modify_plot$RT = factor(modify_plot$RT,levels = c("E", "EM", "M","ML","L"))
+modify_plot$Modify = factor(modify_plot$Modify,levels = c("H3K27ac","H3K4me1","H3K4me3",
+                                                          "H3K27me3","H3K9me2"))
+
+modify_Figure<- modify_plot %>%
+  ggplot(aes(x = Modify, y = percent.fix, fill = RT))+
+  geom_bar( stat = "identity",colour = "black",position = "dodge")+
+  scale_fill_manual(values=c(E = "#2250F1", EM = "#28C5CC", M = "#1A8A12" , ML = "#FFFD33", L = "#FB0018", EL = "#FFEDA0", EML = "#FAB427"))+
+  theme_classic() +ggtitle("Replication Times for Chromatin-Related Features")+theme(plot.title = element_text(hjust = 0.5))+
+  xlab("Histone mark signature")+ylab("% of RT class with mark signature")
+
+modify_Figure                                  
+# ggsave("modify_Figure_NIP.png", modify_Figure , width = 8, height = 5, dpi = 300)
