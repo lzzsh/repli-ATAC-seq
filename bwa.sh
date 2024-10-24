f1a943c383834e923ab0b8e7cdd1a2ad78e7f870
lzzsh <liaozizhuo@westlake.edu.cn>
2024-10-24 22:32:08 +0800

    Create bwa.sh

---
+++ bwa.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#SBATCH -c 8
+#SBATCH --mem=32GB
+#SBATCH -J bwa
+#SBATCH -o bwa.%j.out
+#SBATCH -e bwa.%j.err
+#SBATCH -p intel-sc3,amd-ep2
+#SBATCH -q normal
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=liaozizhuo@westlake.edu.cn
+
+cd /storage/liuxiaodongLab/liaozizhuo/Projects/repli-ATAC-seq/trimmomatic_rawdata
+out="/storage/liuxiaodongLab/liaozizhuo/Projects/repli-ATAC-seq/bwa_all_rawdata"
+for file in $(ls *1P.fq.gz);do
+	prefix=${file%%_*}
+	bwa mem rice_all_genomes_v7_bwa_index -t 10 -M -k 32 -R '@RG\tID:${prefix}\tSM:${prefix}\tLB:${prefix}\tPL:illumina' ${prefix}_1P.fq.gz ${prefix}_2P.fq.gz | samtools view -ubhS > ${out}/${prefix}.bam
+	sleep 1
+done
+
+cd /storage/liuxiaodongLab/liaozizhuo/Projects/repli-ATAC-seq/bwa_all_rawdata
+
+for f in $(ls *bam);do
+	prefix=${f%%\.*}
+	samtools flagstat $f > ${prefix}_flagstat.out
+done
