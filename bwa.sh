fb31b36c2bb233d3f7b326edd556ef954f36a863
lzzsh <liaozizhuo@westlake.edu.cn>
2024-10-25 11:06:38 +0800

    Update bwa.sh

--- bwa.sh
+++ bwa.sh
@@ -9,7 +9,12 @@
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=liaozizhuo@westlake.edu.cn
 
+module load samtools/1.13
+module load bwa/0.7.17
+
 cd /storage/liuxiaodongLab/liaozizhuo/Projects/repli-ATAC-seq/trimmomatic_rawdata
+mkdir ../bwa_all_rawdata
+
 out="/storage/liuxiaodongLab/liaozizhuo/Projects/repli-ATAC-seq/bwa_all_rawdata"
 for file in $(ls *1P.fq.gz);do
 	prefix=${file%%_*}
