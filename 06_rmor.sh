cc0807f80ec92eeffbde88af7621b666352a647d
lzzsh <liaozizhuo@westlake.edu.cn>
2024-10-26 11:37:38 +0800

    Update 06_rmor.sh

--- 06_rmor.sh
+++ 06_rmor.sh
@@ -9,6 +9,8 @@
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=liaozizhuo@westlake.edu.cn
 
+module load samtools/1.13
+
 cd /storage/liuxiaodongLab/liaozizhuo/Projects/repli-ATAC-seq/bwa_all_rawdata
 
 for file in $(ls *_sort_rmdup.bam);do
