7055eb10fbacaf3047e63497117af9a9040c3ee1
lzzsh <liaozizhuo@westlake.edu.cn>
2024-10-26 12:06:35 +0800

    Create 09_convert_bw.sh

---
+++ 09_convert_bw.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#SBATCH -c 8
+#SBATCH --mem=32GB
+#SBATCH -J bws
+#SBATCH -o bws.%j.out
+#SBATCH -e bws.%j.err
+#SBATCH -p intel-sc3,amd-ep2
+#SBATCH -q normal
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=liaozizhuo@westlake.edu.cn
+
+
+cd /storage/liuxiaodongLab/liaozizhuo/Projects/repli-ATAC-seq/bwa_all_rawdata
+mkdir bws
+
+#mkdir bws
+for file in $(find ./ -name '*q30.bam');do
+	name=$(basename ${file})
+	prefix=${name%%_*}
+	bamCoverage --bam ${file} -o ./bws/${prefix}.bw --normalizeUsing BPM -p 5 --binSize 50
+	sleep 1
+done
