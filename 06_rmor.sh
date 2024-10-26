dae598e204619bd0328df5a1f44b9727d1e1c070
lzzsh <liaozizhuo@westlake.edu.cn>
2024-10-26 11:35:40 +0800

    Create 06_rmor.sh

---
+++ 06_rmor.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#SBATCH -c 8
+#SBATCH --mem=32GB
+#SBATCH -J rmor
+#SBATCH -o rmor.%j.out
+#SBATCH -e rmor.%j.err
+#SBATCH -p intel-sc3,amd-ep2
+#SBATCH -q normal
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=liaozizhuo@westlake.edu.cn
+
+cd /storage/liuxiaodongLab/liaozizhuo/Projects/repli-ATAC-seq/bwa_all_rawdata
+
+for file in $(ls *_sort_rmdup.bam);do
+	prefix=${file%\.*}
+	samtools view -h ${file} | grep -v 'chr13\|chr14' | samtools view -bh > ${prefix}_rmor.bam
+	sleep 1
+done
