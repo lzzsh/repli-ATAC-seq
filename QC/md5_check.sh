64e65e38302ecbfa295fa20433b84bca319240c5
lzzsh <liaozizhuo@westlake.edu.cn>
2024-10-28 15:14:22 +0800

    Create md5_check.sh

---
+++ QC/md5_check.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#SBATCH -c 4
+#SBATCH --mem=8GB
+#SBATCH -J md5
+#SBATCH -o md5.%j.out
+#SBATCH -e md5.%j.err
+#SBATCH -p intel-sc3,amd-ep2
+#SBATCH -q normal
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=liaozizhuo@westlake.edu.cn
+
+cd /storage/liuxiaodongLab/liaozizhuo/Projects/repli-ATAC-seq_CR2/01.RawData
+
+for file in *R1.fastq.gz.md5; do
+    prefix=${file%_*}
+    echo "Checking ${prefix}_R1.fastq.gz.md5" >> checksum_results.txt
+    md5sum -c "${prefix}_R1.fastq.gz.md5" >> checksum_results.txt
+    echo "-----------------------------" >> checksum_results.txt
+    echo "Checking ${prefix}_R2.fastq.gz.md5" >> checksum_results.txt
+    md5sum -c "${prefix}_R2.fastq.gz.md5" >> checksum_results.txt
+    echo "-----------------------------" >> checksum_results.txt
+done
