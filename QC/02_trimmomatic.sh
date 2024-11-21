0ff3d5a928e6961e61b73c59ea5b3ed4a11893af
lzzsh <liaozizhuo@westlake.edu.cn>
2024-11-21 09:48:58 +0800

    Update 02_trimmomatic.sh

--- QC/02_trimmomatic.sh
+++ QC/02_trimmomatic.sh
@@ -11,12 +11,37 @@
 
 module load trimmomatic/0.39
 
-cd /storage/liuxiaodongLab/liaozizhuo/Projects/repli-ATAC-seq/
-mkdir trimmomatic_rawdata
+cd /storage/liuxiaodongLab/liaozizhuo/Projects/repli-ATAC-seq
 
-for file in $(ls ./01.RawData/*1.fq.gz);do
-	file=$(basename $file)
-	prefix=${file%_*}
-	trimmomatic PE -threads 10 -phred33 -trimlog ./trimmomatic_rawdata/${prefix}trim.log -basein ./01.RawData/${prefix}_1.fq.gz -baseout ./trimmomatic_rawdata/${prefix}.fq.gz ILLUMINACLIP:/storage/publicdata/trimmomatic_adapters/NexteraPE-PE.fa:2:30:10:8:TRUE SLIDINGWINDOW:4:15 MINLEN:30
-	sleep 1
-done
+# Create output directory
+mkdir -p trimmomatic_rawdata
+
+# Function to rename files
+rename_files() {
+    for file in ./01.RawData/*R1.fastq.gz; do
+        new_file=$(echo $file | sed 's/R1/1/g' | sed 's/fastq/fq/g')
+        mv "$file" "$new_file"
+    done
+
+    for file in ./01.RawData/*R2.fastq.gz; do
+        new_file=$(echo $file | sed 's/R2/2/g' | sed 's/fastq/fq/g')
+        mv "$file" "$new_file"
+    done
+}
+
+# Function to run Trimmomatic
+run_trimmomatic() {
+    for file in $(ls ./01.RawData/*1.fq.gz); do
+        file=$(basename $file)
+        prefix=${file%_*}
+        trimmomatic PE -threads 10 -phred33 -trimlog ./trimmomatic_rawdata/${prefix}trim.log \
+            -basein ./01.RawData/${prefix}_1.fq.gz -baseout ./trimmomatic_rawdata/${prefix}.fq.gz \
+            ILLUMINACLIP:/storage/publicdata/trimmomatic_adapters/NexteraPE-PE.fa:2:30:10:8:TRUE \
+            SLIDINGWINDOW:4:15 MINLEN:30
+        sleep 1
+    done
+}
+
+# Execute renaming and Trimmomatic
+rename_files
+run_trimmomatic
