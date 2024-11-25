74ac8f931575f8440c165dcc59631cc7267f4221
lzzsh <liaozizhuo@westlake.edu.cn>
2024-11-25 10:24:08 +0800

    Update macs2_control.sh

--- peak_calling/macs2_control.sh
+++ peak_calling/macs2_control.sh
@@ -14,36 +14,36 @@ mkdir ../macs2
 out="/storage/liuxiaodongLab/liaozizhuo/Projects/repli-ATAC-seq/macs2"
 
 macs2 callpeak -t /storage/liuxiaodongLab/liaozizhuo/Projects/repli-ATAC-seq_sup/LZZ-adjp_result/merged_bam/bams/LZZ-1-d_S99_sort_rmdup_rmor_q30.bam -c /storage/liuxiaodongLab/liaozizhuo/Projects/repli-ATAC-seq_control/bwa_all_rawdata/LZZ-8-1_S99_sort_rmdup_rmor_q30.bam -n LZZ-1-d \
-        --shift 100 --extsize 200 --nomodel -B --SPMR -g 3.0E8 -q 0.01 --outdir ${out}/LZZ-1-d_out 
+        --shift 100 --extsize 200 --nomodel -B --SPMR -g 3.0E8 -q 0.1 --outdir ${out}/LZZ-1-d_out 
 
 sort -k8,8nr ${out}/LZZ-1-d_out/LZZ-1-d_peaks.narrowPeak > ${out}/LZZ-1-d_out/LZZ-1-d_peaks_sort.narrowPeak
 
 macs2 callpeak -t LZZ-2-e_S99_sort_rmdup_rmor_q30.bam -c /storage/liuxiaodongLab/liaozizhuo/Projects/repli-ATAC-seq_control/bwa_all_rawdata/LZZ-8-2_S99_sort_rmdup_rmor_q30.bam -n LZZ-2-e \
-        --shift 100 --extsize 200 --nomodel -B --SPMR -g 3.0E8 -q 0.01 --outdir ${out}/LZZ-2-e_out 
+        --shift 100 --extsize 200 --nomodel -B --SPMR -g 3.0E8 -q 0.1 --outdir ${out}/LZZ-2-e_out 
 
 sort -k8,8nr ${out}/LZZ-2-e_out/LZZ-2-e_peaks.narrowPeak > ${out}/LZZ-2-e_out/LZZ-2-e_peaks_sort.narrowPeak
 
 macs2 callpeak -t LZZ-1-f_S99_sort_rmdup_rmor_q30.bam -c /storage/liuxiaodongLab/liaozizhuo/Projects/repli-ATAC-seq_control/bwa_all_rawdata/LZZ-8-3_S99_sort_rmdup_rmor_q30.bam -n LZZ-1-f \
-        --shift 100 --extsize 200 --nomodel -B --SPMR -g 3.0E8 -q 0.01 --outdir ${out}/LZZ-1-f_out 
+        --shift 100 --extsize 200 --nomodel -B --SPMR -g 3.0E8 -q 0.1 --outdir ${out}/LZZ-1-f_out 
 
 sort -k8,8nr ${out}/LZZ-1-f_out/LZZ-1-f_peaks.narrowPeak > ${out}/LZZ-1-f_out/LZZ-1-f_peaks_sort.narrowPeak
 
 macs2 callpeak -t ../ZH11_1/origin-2_origin-2-6_sort_rmdup_rmor_q30.bam -c /storage/liuxiaodongLab/liaozizhuo/Projects/repli-ATAC-seq_control/bwa_all_rawdata/LZZ-8-1_S99_sort_rmdup_rmor_q30.bam -n ZH11_1_ES \
-        --shift 100 --extsize 200 --nomodel -B --SPMR -g 3.0E8 -q 0.01 --outdir ${out}/ZH11_1_ES_out
+        --shift 100 --extsize 200 --nomodel -B --SPMR -g 3.0E8 -q 0.1 --outdir ${out}/ZH11_1_ES_out
 
 sort -k8,8nr ${out}/ZH11_1_ES_out/ZH11_1_ES_peaks.narrowPeak > ${out}/ZH11_1_ES_out/ZH11_1_ES_peaks_sort.narrowPeak
 
 macs2 callpeak -t ../ZH11_1/origin-2_origin-2-4_sort_rmdup_rmor_q30.bam -c /storage/liuxiaodongLab/liaozizhuo/Projects/repli-ATAC-seq_control/bwa_all_rawdata/LZZ-8-2_S99_sort_rmdup_rmor_q30.bam -n ZH11_1_MS \
-        --shift 100 --extsize 200 --nomodel -B --SPMR -g 3.0E8 -q 0.01 --outdir ${out}/ZH11_1_MS_out
+        --shift 100 --extsize 200 --nomodel -B --SPMR -g 3.0E8 -q 0.1 --outdir ${out}/ZH11_1_MS_out
 
 sort -k8,8nr ${out}/ZH11_1_MS_out/ZH11_1_MS_peaks.narrowPeak > ${out}/ZH11_1_MS_out/ZH11_1_MS_peaks_sort.narrowPeak
 
 macs2 callpeak -t ../ZH11_1/origin-2_origin-2-5_sort_rmdup_rmor_q30.bam -c /storage/liuxiaodongLab/liaozizhuo/Projects/repli-ATAC-seq_control/bwa_all_rawdata/LZZ-8-3_S99_sort_rmdup_rmor_q30.bam -n ZH11_1_LS \
-        --shift 100 --extsize 200 --nomodel -B --SPMR -g 3.0E8 -q 0.01 --outdir ${out}/ZH11_1_LS_out
+        --shift 100 --extsize 200 --nomodel -B --SPMR -g 3.0E8 -q 0.1 --outdir ${out}/ZH11_1_LS_out
 
 sort -k8,8nr ${out}/ZH11_1_LS_out/ZH11_1_LS_peaks.narrowPeak > ${out}/ZH11_1_LS_out/ZH11_1_LS_peaks_sort.narrowPeak
 
 macs2 callpeak -t ../01.RawData/origin-origin-6_sort_rmdup_rmor_q30.bam -c /storage/liuxiaodongLab/liaozizhuo/Projects/repli-ATAC-seq_control/bwa_all_rawdata/LZZ-8-1_S99_sort_rmdup_rmor_q30.bam -n NIP_ES \
-        --shift 100 --extsize 200 --nomodel -B --SPMR -g 3.0E8 -q 0.01 --outdir ${out}/NIP_ES_out
+        --shift 100 --extsize 200 --nomodel -B --SPMR -g 3.0E8 -q 0.1 --outdir ${out}/NIP_ES_out
 
 sort -k8,8nr ${out}/NIP_ES_out/NIP_ES_peaks.narrowPeak > ${out}/NIP_ES_out/NIP_ES_peaks_sort.narrowPeak
