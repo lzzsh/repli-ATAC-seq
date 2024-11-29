f04e32d115d2b8cc1715389a1fa573f6f107e095
lzzsh <liaozizhuo@westlake.edu.cn>
2024-11-29 20:17:50 +0800

    Update heatmap_tss.R

--- Figures/heatmap_tss.R
+++ Figures/heatmap_tss.R
@@ -1,24 +1,26 @@
 # Find peaks in each S phage
 gff<-read.table("~/Desktop/Rfiles/idr_peaks/TSS_RT.bed",comment.char = "")
+colnames(gff)[c(1,2,3,7)] <- c("chr","start","end","RT")
 
 ES <- gff %>%
-  filter(RT == "E") %>%
-  select(chr,start,end)
+  filter(RT == "E") 
+
 EMS <- gff %>%
   filter(RT == "EM") %>%
-  select(chr,start,end)
+
 MS <- gff %>%
   filter(RT == "M") %>%
-  select(chr,start,end)
+
 MLS <- gff %>%
   filter(RT == "ML") %>%
-  select(chr,start,end)
+
 LS <- gff %>%
   filter(RT == "L") %>%
-  select(chr,start,end)
 
-write.table(ES,"~/Documents/GitHub/repli-ATAC-seq/output/heatmap/ES_peaks.txt",row.names = F,quote=F,sep = "\t",col.names = F)
-write.table(EMS,"~/Documents/GitHub/repli-ATAC-seq/output/heatmap/EMS_peaks.txt",row.names = F,quote=F,sep = "\t",col.names = F)
-write.table(MS,"~/Documents/GitHub/repli-ATAC-seq/output/heatmap/MS_peaks.txt",row.names = F,quote=F,sep = "\t",col.names = F)
-write.table(MLS,"~/Documents/GitHub/repli-ATAC-seq/output/heatmap/MLS_peaks.txt",row.names = F,quote=F,sep = "\t",col.names = F)
-write.table(LS,"~/Documents/GitHub/repli-ATAC-seq/output/heatmap/LS_peaks.txt",row.names = F,quote=F,sep = "\t",col.names = F)
+
+write.table(ES,"~/Documents/GitHub/repli-ATAC-seq/output/heatmap/ES_tss.txt",row.names = F,quote=F,sep = "\t",col.names = F)
+
+write.table(EMS,"~/Documents/GitHub/repli-ATAC-seq/output/heatmap/EMS_tss.txt",row.names = F,quote=F,sep = "\t",col.names = F)
+write.table(MS,"~/Documents/GitHub/repli-ATAC-seq/output/heatmap/MS_tss.txt",row.names = F,quote=F,sep = "\t",col.names = F)
+write.table(MLS,"~/Documents/GitHub/repli-ATAC-seq/output/heatmap/MLS_tss.txt",row.names = F,quote=F,sep = "\t",col.names = F)
+write.table(LS,"~/Documents/GitHub/repli-ATAC-seq/output/heatmap/LS_tss.txt",row.names = F,quote=F,sep = "\t",col.names = F)
