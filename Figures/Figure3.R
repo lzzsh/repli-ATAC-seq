4bd83f49319722483727192b84d6d4c0f9552e44
lzzsh <liaozizhuo@westlake.edu.cn>
2024-11-30 13:57:21 +0800

    Update Figure3.R

--- Figures/Figure3.R
+++ Figures/Figure3.R
@@ -1,14 +1,18 @@
 library(ChIPseeker)
 library(ggplot2)
+library(ggimage)
+library(ggupset)
+library(GenomicFeatures)
+library(ggpubr)
+
+# load 6 files
 files <- getSampleFiles()
 files <- list.files("~/Desktop/Rfiles/peaks/")
 setwd("~/Desktop/Rfiles/peaks")
 peak=GenomicRanges::GRangesList(ES=readPeakFile(files[4]),MS=readPeakFile(files[5]),LS=readPeakFile(files[6]))
 covplot(peak, weightCol="V5",chrs=c("chr1","chr2","chr3","chr4","chr5","chr6","chr7","chr8","chr9","chr10","chr11","chr12"))+ 
   facet_grid(chr ~ .id)
-library(ggimage)
-library(ggupset)
-library(GenomicFeatures)
+
 ricegff<-makeTxDbFromGFF("F:/毕设/Arabidopsis/all_DIY.gff3")
 peakAnno1 <- annotatePeak(files[[4]], 
                          tssRegion=c(-3000, 3000),
@@ -24,9 +28,6 @@ peakAnno3 <- annotatePeak(files[[6]],
 plotAnnoPie(peakAnno3)
 
 
-
-
-library(ggpubr)
 annos<-ggarrange(anno3, anno1, anno2, 
           labels = c("ES", "MS", "LS"),
           ncol = 2, nrow = 2)
