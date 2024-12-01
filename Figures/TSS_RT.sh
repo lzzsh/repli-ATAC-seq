3783ed5ac17a629a4af7e8112c083b7fa5a8aed9
lzzsh <liaozizhuo@westlake.edu.cn>
2024-12-01 12:24:16 +0800

    Update TSS_RT.sh

--- Figures/TSS_RT.sh
+++ Figures/TSS_RT.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 awk '$3 == "gene"' all_DIY.gff3|awk 'BEGIN{FS="\t|=|;";OFS="\t"}{print $1,$4-1,$4,$3,$6,$7}'>TSS.bed
 bedtools intersect -a TSS.bed -b ZH11_RT.gff3 -wa -wb > TSS_RT.bed
 cut -f 1,2,3,4,5,6,10 TSS_RT.bed > TSS_RT_temp.bed
