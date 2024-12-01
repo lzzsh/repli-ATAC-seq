1615da2df7449787390fe1524bf15e0f7070d531
lzzsh <liaozizhuo@westlake.edu.cn>
2024-12-01 13:10:06 +0800

    Update unannotated_RT.sh

--- Figures/unannotated_RT.sh
+++ Figures/unannotated_RT.sh
@@ -5,7 +5,7 @@ sort -k1,1 -k2,2n ZH11_RT.gff3 > ZH11_RT_sorted.gff3
 sort -k1,1 -k2,2n annotate_RT.bed > annotate_RT_sorted.bed
 
 # Use bedtools subtract to find unannotated regions
-bedtools subtract -a ZH11_RT_sorted.gff3 -b annotate_RT_sorted.bed > unannotated.bed
+bedtools intersect -a ZH11_RT_sorted.gff3 -b annotate_RT_sorted.bed -v > unannotated_RT.bed
 
 # Delete the intermediate files
 rm ZH11_RT_sorted.gff3
