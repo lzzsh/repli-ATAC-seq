c6524b423d137f1ecd80a1c0a62987fb63fe5719
lzzsh <liaozizhuo@westlake.edu.cn>
2024-12-01 12:23:33 +0800

    Create unannotated_RT.sh

---
+++ Figures/unannotated_RT.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Sort the files
+sort -k1,1 -k2,2n ZH11_RT.gff3 > ZH11_RT_sorted.gff3
+sort -k1,1 -k2,2n annotate_RT.bed > annotate_RT_sorted.bed
+
+# Use bedtools subtract to find unannotated regions
+bedtools subtract -a ZH11_RT_sorted.gff3 -b annotate_RT_sorted.bed > unannotated.bed
+
+# Delete the intermediate files
+rm ZH11_RT_sorted.gff3
+rm annotate_RT_sorted.bed
