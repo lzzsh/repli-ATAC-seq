017adf2ae6a2fd0291e8034bd686b47401b17f3f
lzzsh <liaozizhuo@westlake.edu.cn>
2024-12-02 16:15:35 +0800

    Update Figure1.R

--- Figures/Figure1.R
+++ Figures/Figure1.R
@@ -3,17 +3,17 @@ library(ggplot2)
 library(ggstatsplot)
 
 # load gff which contains the RT location of each open chromatin region
-gff<-read.table("~/Desktop/Rfiles/segamentation/rbq_gff.gff3",skip = 2,comment.char = "")
-gff<-gff[,c(1,4,5,9)]
+gff <- read.table("~/Desktop/Rfiles/segamentation/rbq_gff.gff3",skip = 2,comment.char = "")
+gff <- gff[,c(1,4,5,9)]
 colnames(gff)<-c("chr","start","end","RT")
-gff$RT<-lapply(gff$RT, function(x) unlist(strsplit(x,";"))[1])
-gff$RT<-lapply(gff$RT, function(x) unlist(strsplit(x,"="))[2])
-gff<-gff[ !(gff$chr %in% c("chrUn","chrSy")),] 
-gff$length<-gff$end-gff$start+1
-gff[,4]<-gsub("S","",gff[,4])
-RT_length<-gff %>% group_by(RT)%>%
+gff$RT <- lapply(gff$RT, function(x) unlist(strsplit(x,";"))[1])
+gff$RT <- lapply(gff$RT, function(x) unlist(strsplit(x,"="))[2])
+gff <- gff[ !(gff$chr %in% c("chrUn","chrSy")),] 
+gff$length <- gff$end-gff$start+1
+gff[,4] <- gsub("S","",gff[,4])
+RT_length <- gff %>% group_by(RT) %>%
   summarise(Sum = sum(length))
-RT_length$freq<-RT_length[,2]/sum(RT_length[,2])
+RT_length$freq <- RT_length[,2]/sum(RT_length[,2])
 
 # pie chart
 gff$RT <- factor(gff$RT,levels = c("EML","EL","L","ML","M","EM","E"))
@@ -29,7 +29,7 @@ p5 <- ggpiestats(gff, 'RT',
 p5
 # ggsave("~/Desktop/photo/1.png", p5 , width = 8, height = 5, dpi = 300)                 
 
-gff$length<-log2((gff$end-gff$start+1))
+gff$length <- log2((gff$end-gff$start+1))
 
 # boxplot
 gff$RT <- factor(gff$RT,levels = c("E","EM","M","ML","L","EL","EML"))
