63324888e0642f31deb81ec65402ae84e22436d1
lzzsh <liaozizhuo@westlake.edu.cn>
2024-12-03 15:11:52 +0800

    Update identify_peaks.sh

--- peak_calling/identify_peaks.sh
+++ peak_calling/identify_peaks.sh
@@ -26,7 +26,7 @@ awk '{if($5 >= 415) print $0}' LS-idr > LS-idr_0.1
 # sort and merge peaks
 cat ES-idr_0.1 MS-idr_0.1 LS-idr_0.1 > EdU-idr_0.1
 sort -k1,1 -k2,2n EdU-idr_0.1 > EdU-idr_0.1_sort
-bedtools merge -i EdU-idr_0.1_sort > EdU-idr_0.1_sort_merge
+bedtools merge -i EdU-idr_0.1_sort > EdU-idr_0.1_control_sort_merge
 
 # calculate reads number in each open chromatin region
 location="/storage/liuxiaodongLab/liaozizhuo/Projects/repli-ATAC-seq/01.RawData"
@@ -34,4 +34,4 @@ bedtools multicov -bams \
 ${location}/origin-2_origin-2-4_sort_rmdup_rmor_q30.bam \
 ${location}/origin-2_origin-2-5_sort_rmdup_rmor_q30.bam \
 ${location}/origin-2_origin-2-6_sort_rmdup_rmor_q30.bam \
--bed EdU-idr_0.1_sort_merge > EdU-idr_reads_ZH11.txt
+-bed EdU-idr_0.1_control_sort_merge > EdU-idr_reads_ZH11_control.txt
