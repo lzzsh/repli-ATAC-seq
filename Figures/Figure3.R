316a9949f39f091a1332e77be90ea67b6f97a157
lzzsh <liaozizhuo@westlake.edu.cn>
2024-12-03 19:13:00 +0800

    Update Figure3.R

--- Figures/Figure3.R
+++ Figures/Figure3.R
@@ -5,39 +5,56 @@ library(ggupset)
 library(GenomicFeatures)
 library(ggpubr)
 
+options(ChIPseeker.ignore_1st_exon = T)
+options(ChIPseeker.ignore_1st_intron = T)
+options(ChIPseeker.ignore_downstream = T)
+options(ChIPseeker.ignore_promoter_subcategory = F)
+
 # load 6 files
-files <- getSampleFiles()
 files <- list.files("~/Desktop/Rfiles/peaks/")
 setwd("~/Desktop/Rfiles/peaks")
-peak=GenomicRanges::GRangesList(ES=readPeakFile(files[4]),MS=readPeakFile(files[5]),LS=readPeakFile(files[6]))
-covplot(peak, weightCol="V5",chrs=c("chr1","chr2","chr3","chr4","chr5","chr6","chr7","chr8","chr9","chr10","chr11","chr12"))+ 
-  facet_grid(chr ~ .id)
 
 ricegff<-makeTxDbFromGFF("~/Desktop/Rfiles/all_DIY.gff3")
-peakAnno1 <- annotatePeak(files[[4]], 
-                         tssRegion=c(-3000, 3000),
-                         TxDb=ricegff, annoDb=ricegff)
+
+# NIP
+peakAnno1 <- annotatePeak(files[[1]], 
+                          tssRegion=c(-3000, 3000),
+                          TxDb=ricegff, annoDb=ricegff,
+                          genomicAnnotationPriority = c("Promoter", "5UTR", "3UTR", "Exon", "Intron", "Downstream", "Intergenic"))
 plotAnnoPie(peakAnno1)
-peakAnno2 <- annotatePeak(files[[5]], 
-                         tssRegion=c(-3000, 3000),
-                         TxDb=ricegff, annoDb=ricegff)
+peakAnno2 <- annotatePeak(files[[3]], 
+                          tssRegion=c(-3000, 3000),
+                          TxDb=ricegff, annoDb=ricegff,
+                          genomicAnnotationPriority = c("Promoter", "5UTR", "3UTR", "Exon", "Intron", "Downstream", "Intergenic"))
 plotAnnoPie(peakAnno2)
-peakAnno3 <- annotatePeak(files[[6]], 
-                         tssRegion=c(-3000, 3000),
-                         TxDb=ricegff, annoDb=ricegff)
+peakAnno3 <- annotatePeak(files[[2]], 
+                          tssRegion=c(-3000, 3000),
+                          TxDb=ricegff, annoDb=ricegff,
+                          genomicAnnotationPriority = c("Promoter", "5UTR", "3UTR", "Exon", "Intron", "Downstream", "Intergenic"))
 plotAnnoPie(peakAnno3)
 
+# ZH11
+peakAnno1 <- annotatePeak(files[[4]], 
+                          tssRegion=c(-3000, 3000),
+                          TxDb=ricegff, annoDb=ricegff,
+                          genomicAnnotationPriority = c("Promoter", "5UTR", "3UTR", "Exon", "Intron", "Downstream", "Intergenic"))
+plotAnnoPie(peakAnno1)
 
-annos<-ggarrange(anno3, anno1, anno2, 
-          labels = c("ES", "MS", "LS"),
-          ncol = 2, nrow = 2)
+peakAnno2 <- annotatePeak(files[[6]], 
+                          tssRegion=c(-3000, 3000),
+                          TxDb=ricegff, annoDb=ricegff,
+                          genomicAnnotationPriority = c("Promoter", "5UTR", "3UTR", "Exon", "Intron", "Downstream", "Intergenic"))
+plotAnnoPie(peakAnno2)
+peakAnno3 <- annotatePeak(files[[5]], 
+                          tssRegion=c(-3000, 3000),
+                          TxDb=ricegff, annoDb=ricegff,
+                          genomicAnnotationPriority = c("Promoter", "5UTR", "3UTR", "Exon", "Intron", "Downstream", "Intergenic"))
+plotAnnoPie(peakAnno3)
 
-
+# bar plot
 peakAnnoList <- lapply(files, annotatePeak, TxDb=ricegff,tssRegion=c(-3000, 3000), verbose=FALSE)
-for (i in 1:length(peakAnno1)){
-  names(data)[i] <- paste("data_", i, sep = "")
-}
-names(peakAnnoList) <- c("Mid S","G2","G1","Mid S(EdU)","Late S","Early S")
+names(peakAnnoList) <- c("NIP_ES","NIP_MS","NIP_LS","Early S","Late S","Mid S(EdU)","G1","G2","Mid S")
 peakAnnoList<-peakAnnoList[c("G1","Mid S","G2","Early S","Mid S(EdU)","Late S")]
 png5<-plotDistToTSS(peakAnnoList,title = "Distribution of peaks relative to TSS")
+png5
 # ggsave("5.png",png5, width = 8, height = 5, dpi = 600)
