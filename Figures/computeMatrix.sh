0b4eeb628e7c8d3c4c9b6a8e105c71494e9766f2
lzzsh <liaozizhuo@westlake.edu.cn>
2024-12-03 16:22:42 +0800

    Update computeMatrix.sh

--- Figures/computeMatrix.sh
+++ Figures/computeMatrix.sh
@@ -27,6 +27,6 @@ computeMatrix reference-point  -b 1000 -a 1000 -R ES_control_tss.txt EMS_control
 /storage/liuxiaodongLab/liaozizhuo/Projects/repli-ATAC-seq_control/bwa_all_rawdata/bws/LZZ-8-1.bw /storage/liuxiaodongLab/liaozizhuo/Projects/repli-ATAC-seq_control/bwa_all_rawdata/bws/LZZ-8-2.bw /storage/liuxiaodongLab/liaozizhuo/Projects/repli-ATAC-seq_control/bwa_all_rawdata/bws/LZZ-8-2.bw \
 --skipZeros -o matrix_peaks_control.gz --outFileSortedRegions regions_peaks_control.bed
 
-plotProfile -m matrix_peaks_control.gz  -out peak_profile_control.pdf  --refPointLabel TSS --regionsLabel E EM M ML L --outFileSortedRegions location_control.bed --plotFileFormat pdf --perGroup
-plotHeatmap -m matrix_peaks_control.gz  -out peak_heatmap_control.pdf  --refPointLabel TSS --regionsLabel E EM M ML L --outFileSortedRegions location_control.bed --plotFileFormat pdf --perGroup \
+plotProfile -m matrix_peaks_control.gz  -out TSS_profile_control.pdf  --refPointLabel TSS --regionsLabel E EM M ML L --outFileSortedRegions location_control.bed --plotFileFormat pdf --perGroup
+plotHeatmap -m matrix_peaks_control.gz  -out TSS_heatmap_control.pdf  --refPointLabel TSS --regionsLabel E EM M ML L --outFileSortedRegions location_control.bed --plotFileFormat pdf --perGroup \
 --samplesLabel G1 MS_without_EdU G2 ES MS LS ES_control MS_control LS_control --heatmapHeight 35 --heatmapWidth 5
