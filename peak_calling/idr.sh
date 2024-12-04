fb0184a11d1ee811daf5962b2371116d6e70a0a8
lzzsh <liaozizhuo@westlake.edu.cn>
2024-12-04 13:49:41 +0800

    Update idr.sh

--- peak_calling/idr.sh
+++ peak_calling/idr.sh
@@ -9,23 +9,28 @@
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=liaozizhuo@westlake.edu.cn
 
-cd /storage/liuxiaodongLab/liaozizhuo/Projects/repli-ATAC-seq/
+threshold=0.1
+type='p'
+mkdir /storage/liuxiaodongLab/liaozizhuo/Projects/repli-ATAC-seq/idr_result
+input="/storage/liuxiaodongLab/liaozizhuo/Projects/repli-ATAC-seq/macs2/macs2_noctrl_${type}${threshold}"
 out="/storage/liuxiaodongLab/liaozizhuo/Projects/repli-ATAC-seq/idr_result"
 
-idr --samples ZH11_1/macs2/origin-2_origin-2-1_sort_rmdup_rmor_out/origin-2_origin-2-1_sort_rmdup_rmor_peaks_sort.narrowPeak \
-			  macs2/LZZ-1-d-sup_out/LZZ-1-d-sup_peaks_sort.narrowPeak --input-file-type narrowPeak --rank p.value --output-file ${out}/G1-idr --plot --log-output-file ${out}/G1.idr.log
+# ZH11-1 and ZH11-2
+# idr --samples ${input}/NIP_ES_out/NIP_ES_peaks_sort.narrowPeak \
+# 			  ${input}/LZZ-1-d_out/LZZ-1-d_peaks_sort.narrowPeak --input-file-type narrowPeak --rank p.value --output-file ${out}/ES-idr --plot --log-output-file ${out}/ES.idr.log
 
-idr --samples ZH11_1/macs2/origin-2_origin-2-2_sort_rmdup_rmor_out/origin-2_origin-2-2_sort_rmdup_rmor_peaks_sort.narrowPeak \
-			  macs2/LZZ-1-e-sup_out/LZZ-1-e-sup_peaks_sort.narrowPeak --input-file-type narrowPeak --rank p.value --output-file ${out}/MS_withouEdU-idr --plot --log-output-file ${out}/MS_withouEdU.idr.log
+# idr --samples ${input}/ZH11_1_MS_out/ZH11_1_MS_peaks_sort.narrowPeak \
+# 			  ${input}/LZZ-2-e_out/LZZ-2-e_peaks_sort.narrowPeak --input-file-type narrowPeak --rank p.value --output-file ${out}/MS-idr --plot --log-output-file ${out}/MS.idr.log
 
-idr --samples ZH11_1/macs2/origin-2_origin-2-3_sort_rmdup_rmor_out/origin-2_origin-2-3_sort_rmdup_rmor_peaks_sort.narrowPeak \
-			  macs2/LZZ-1-f-sup_out/LZZ-1-f-sup_peaks_sort.narrowPeak --input-file-type narrowPeak --rank p.value --output-file ${out}/G2-idr --plot --log-output-file ${out}/G2.idr.log
+# idr --samples ${input}/ZH11_1_LS_out/ZH11_1_LS_peaks_sort.narrowPeak \
+# 			  ${input}/LZZ-1-f_out/LZZ-1-f_peaks_sort.narrowPeak --input-file-type narrowPeak --rank p.value --output-file ${out}/LS-idr --plot --log-output-file ${out}/LS.idr.log
 
-idr --samples ZH11_1/macs2/origin-2_origin-2-4_sort_rmdup_rmor_out/origin-2_origin-2-4_sort_rmdup_rmor_peaks_sort.narrowPeak \
-			  macs2/LZZ-1-d_out/LZZ-1-d_peaks_sort.narrowPeak --input-file-type narrowPeak --rank p.value --output-file ${out}/ES-idr --plot --log-output-file ${out}/ES.idr.log
+# ZH11-1 and NIP
+idr --samples ${input}/ZH11_1_ES_out/ZH11_1_ES_peaks_sort.narrowPeak \
+                          ${input}/NIP_ES_out/NIP_ES_peaks_sort.narrowPeak --input-file-type narrowPeak --rank p.value --output-file ${out}/ES-idr --plot --log-output-file ${out}/ES.idr.log
 
-idr --samples ZH11_1/macs2/origin-2_origin-2-5_sort_rmdup_rmor_out/origin-2_origin-2-5_sort_rmdup_rmor_peaks_sort.narrowPeak \
-			  macs2/LZZ-2-e_out/LZZ-2-e_peaks_sort.narrowPeak --input-file-type narrowPeak --rank p.value --output-file ${out}/MS-idr --plot --log-output-file ${out}/MS.idr.log
+idr --samples ${input}/ZH11_1_MS_out/ZH11_1_MS_peaks_sort.narrowPeak \
+                          ${input}/NIP_MS_out/NIP_MS_peaks_sort.narrowPeak --input-file-type narrowPeak --rank p.value --output-file ${out}/MS-idr --plot --log-output-file ${out}/MS.idr.log
 
-idr --samples ZH11_1/macs2/origin-2_origin-2-6_sort_rmdup_rmor_out/origin-2_origin-2-6_sort_rmdup_rmor_peaks_sort.narrowPeak \
-			  macs2/LZZ-1-f_out/LZZ-1-f_peaks_sort.narrowPeak --input-file-type narrowPeak --rank p.value --output-file ${out}/LS-idr --plot --log-output-file ${out}/LS.idr.log
+idr --samples ${input}/ZH11_1_LS_out/ZH11_1_LS_peaks_sort.narrowPeak \
+                          ${input}/NIP_LS_out/NIP_LS_peaks_sort.narrowPeak --input-file-type narrowPeak --rank p.value --output-file ${out}/LS-idr --plot --log-output-file ${out}/LS.idr.log
