2786d3d56dd0d180b6efd8ca28fe1c5f8702ffb4
lzzsh <liaozizhuo@westlake.edu.cn>
2024-12-05 14:41:19 +0800

    Update Figure2B.R

--- Figures/Figure2B.R
+++ Figures/Figure2B.R
@@ -2,13 +2,13 @@ library(dplyr)
 library(ggplot2)
 
 # load histone_RT.bed file 
-peaks_reads <- read.table("~/Desktop/Rfiles/idr_peaks/peaks_reads.txt", header = T)
-histone <- read.table("~/Desktop/Rfiles/peak_unit/Histone_RT.bed")
+peaks_reads <- read.table("~/Desktop/Rfiles/idr_peaks/peaks_reads_2_control.txt", header = T, sep = "\t")
+histone <- read.table("~/Desktop/Rfiles/peak_unit/Histone_RT_2.bed", sep = "\t")
 colnames(histone)[1:3]<- c("chr","start","end")
 
 histone <- merge(histone,peaks_reads[,c("chr","start","end","RT")],by=c("chr","start","end")) 
 histone <- histone %>%
-  select(4,5,6,7,9)%>%
+  dplyr::select(5,6,7,8,10)%>%
   filter(!(RT %in% c("EL","EML")))%>%
   setNames(c("chr","start","end","Modify","RT")) %>%
   arrange(across(everything()))
@@ -25,11 +25,11 @@ histone_freq <- as.data.frame(table(histone$Modify,histone$RT)) %>%
   reframe(RT = RT, percent = Freq / sum(Freq))
 
 modify_plot <- merge(histone_freq, RT_freq, by = "RT") %>%
-  select(1,2,3,5) %>%
+  dplyr::select(1,2,3,5) %>%
   mutate(percent.fix = percent.x / percent.y)
 
 modify_plot$RT = factor(modify_plot$RT,levels = c("E", "EM", "M","ML","L"))
-modify_plot$Modify = factor(modify_plot$Modify,levels = c("H3K27ac","H3K4me1","H3K4me3",
+modify_plot$Modify = factor(modify_plot$Modify,levels = c("H3K4me1","H3K27ac","H3K4me3",
                                                           "H3K27me3","H3K9me2"))
 
 # plot
@@ -40,4 +40,4 @@ modify_Figure<- modify_plot %>%
   theme_classic() +ggtitle("Replication Times for Chromatin-Related Features")+theme(plot.title = element_text(hjust = 0.5))+
   xlab("Histone mark signature")+ylab("% of RT class with mark signature")
 modify_Figure                                  
-# ggsave("modify_Figure_NIP.png", modify_Figure , width = 8, height = 5, dpi = 300)
+ggsave("~/Documents/Github/repli-ATAC-seq/output/Figures/Figure2B.pdf", modify_Figure , width = 8, height = 5)
