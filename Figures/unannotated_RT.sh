a4e9eee0b5ce8938f7bd873e2f531212dc709ffc
lzzsh <liaozizhuo@westlake.edu.cn>
2024-12-05 16:37:50 +0800

    Update unannotated_RT.sh

--- Figures/unannotated_RT.sh
+++ Figures/unannotated_RT.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# IDR_peaks
 # Sort the files
 sort -k1,1 -k2,2n ZH11_RT.gff3 > ZH11_RT_sorted.gff3
 sort -k1,1 -k2,2n annotate_RT.bed > annotate_RT_sorted.bed
@@ -10,3 +11,16 @@ bedtools intersect -a ZH11_RT_sorted.gff3 -b annotate_RT_sorted.bed -v > unannot
 # Delete the intermediate files
 rm ZH11_RT_sorted.gff3
 rm annotate_RT_sorted.bed
+
+
+# ZH11-2_peaks
+# Sort the files
+sort -k1,1 -k2,2n ZH11_2_RT_control.gff3 > ZH11_2_RT_sorted.gff3
+sort -k1,1 -k2,2n annotate_RT_2.bed > annotate_RT_2_sorted.bed
+
+# Use bedtools subtract to find unannotated regions
+bedtools intersect -a ZH11_2_RT_sorted.gff3 -b annotate_RT_2_sorted.bed -v > unannotated_RT_2.bed
+
+# Delete the intermediate files
+rm ZH11_2_RT_sorted.gff3
+rm annotate_RT_2_sorted.bed
