fa2450ddce3d999f333b7b12471e05202d8adb39
lzzsh <liaozizhuo@westlake.edu.cn>
2024-12-05 16:37:44 +0800

    Update histone_RT.sh

--- Figures/histone_RT.sh
+++ Figures/histone_RT.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
+
+# IDR_peaks
 cut -f 1,2,3,9,10 Histone.bed > Histone_temp.bed
 mv Histone_temp.bed Histone.bed 
 awk 'BEGIN {FS=OFS="\t"} {if ($1 ~ /^chr[1-9]$/) $1="chr0" substr($1, 4); print}' Histone.bed > Histone_temp.bed
 mv Histone_temp.bed Histone.bed 
 bedtools intersect -a ZH11.gff3 -b Histone.bed -wa -wb > Histone_RT.bed
 
+# ZH11-2_peaks
+bedtools intersect -a ZH11_2_RT_control.gff3 -b Histone.bed -wa -wb > Histone_RT_2.bed
