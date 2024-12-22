57cbc74c00405b15b374531a8cfe0de4b5b470c7
lzzsh <liaozizhuo@westlake.edu.cn>
2024-12-22 19:04:41 +0800

    Update cut_RT_top1000.sh

--- peak_calling/cut_RT_top1000.sh
+++ peak_calling/cut_RT_top1000.sh
@@ -2,6 +2,12 @@ sort -k8,8gr ../x39_cut_out/x39_cut_peaks.narrowPeak | head -n 3000 > ../x39_cut
 sort -k8,8gr ../x49_cut_out/x49_cut_peaks.narrowPeak | head -n 3000 > ../x49_cut_out/x49_cut_peaks_top3000.narrowPeak
 sort -k8,8gr ../xw11_cut_out/xw11_cut_peaks.narrowPeak | head -n 3000 > ../xw11_cut_out/xw11_cut_peaks_top3000.narrowPeak
 
+# cuttag site
 bedtools intersect -a ZH11_2_RT_control.gff3 -b ../x39_cut_out/x39_cut_peaks_top3000.narrowPeak -wa -wb | awk 'BEGIN{FS=OFS="\t"} {print $5, $6+$14, $6+$14+1, $4}' > ../x39_cut_out/x39_RT_top3000.bed
 bedtools intersect -a ZH11_2_RT_control.gff3 -b ../x49_cut_out/x49_cut_peaks_top3000.narrowPeak -wa -wb | awk 'BEGIN{FS=OFS="\t"} {print $5, $6+$14, $6+$14+1, $4}' > ../x49_cut_out/x49_RT_top3000.bed
 bedtools intersect -a ZH11_2_RT_control.gff3 -b ../xw11_cut_out/xw11_cut_peaks_top3000.narrowPeak -wa -wb | awk 'BEGIN{FS=OFS="\t"} {print $5, $6+$14, $6+$14+1, $4}' > ../xw11_cut_out/xw11_RT_top3000.bed
+
+# 
+bedtools intersect -a ZH11_2_RT_control.gff3 -b ../x39_cut_out/x39_cut_peaks_top1000.narrowPeak -wa -wb > ../x39_cut_out/x39_RT_top1000.bed
+bedtools intersect -a ZH11_2_RT_control.gff3 -b ../x49_cut_out/x49_cut_peaks_top1000.narrowPeak -wa -wb > ../x49_cut_out/x39_RT_top1000.bed
+bedtools intersect -a ZH11_2_RT_control.gff3 -b ../xw11_cut_out/xw11_cut_peaks_top1000.narrowPeak -wa -wb > ../xw11_cut_out/x39_RT_top1000.bed
