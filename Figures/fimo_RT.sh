2e8fe72129c7926943cc11720116a021d89743d5
lzzsh <liaozizhuo@westlake.edu.cn>
2024-12-22 12:01:49 +0800

    Update fimo_RT.sh

--- Figures/fimo_RT.sh
+++ Figures/fimo_RT.sh
@@ -1,3 +1,7 @@
+# select symbol from txt
+cat JASPAR2024_CORE_plants_non-redundant_pfms_meme.txt| grep MOTIF | cut -d " " -f 2,3 > symbol.txt
+
+# classification
 awk 'NR>1 {print $2, $3, $4, $1, $5, $6, $7, $8, $9, $10}' OFS='\t' fimo.txt > fimo_temp.txt
 mv fimo_temp.txt fimo.txt
 sed 's/\t*$//' fimo.txt > fimo_temp.txt
