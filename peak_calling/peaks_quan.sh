0110ac7b9d356015e8ef906518ac53ecc2fc9b8f
lzzsh <liaozizhuo@westlake.edu.cn>
2025-01-08 09:57:42 +0800

    Update peaks_quan.sh

--- peak_calling/peaks_quan.sh
+++ peak_calling/peaks_quan.sh
@@ -1,12 +1,12 @@
 # sort and merge peaks
-cat ../ZH11_3_ES_out/ZH11_3_ES_peaks.narrowPeak ../ZH11_3_MS_out/ZH11_3_MS_peaks_sort.narrowPeak ../ZH11_3_LS_out/ZH11_3_LS_peaks_sort.narrowPeak > ZH11_3_peaks_p0.01
-cat ../ZH11_2_ES_out/ZH11_2_ES_peaks.narrowPeak ../ZH11_2_MS_out/ZH11_2_MS_peaks_sort.narrowPeak ../ZH11_2_LS_out/ZH11_2_LS_peaks_sort.narrowPeak > ZH11_2_peaks_p0.01
-cat ZH11_3_peaks_p0.01 ZH11_2_peaks_p0.01 > peaks_p0.01
-sort -k1,1 -k2,2n peaks_p0.01 > peaks_p0.01_sort
-bedtools merge -i peaks_p0.01_sort > merged_peaks_p0.01_sort
+awk '{print $1"\t"$2"\t"$3}' *.txt > all_peaks.txt
+sort -k1,1 -k2,2n all_peaks.txt | bedtools merge -i - > merged_peaks.bed
 
 # select reads only from open chromatin region
 files=(
+	"/storage/liuxiaodongLab/liaozizhuo/Projects/repli-ATAC-seq_control/bwa_all_rawdata/LZZ-9-1_S99_sort_rmdup_rmor_q30.bam"
+	"/storage/liuxiaodongLab/liaozizhuo/Projects/repli-ATAC-seq_control/bwa_all_rawdata/LZZ-9-2_S99_sort_rmdup_rmor_q30.bam"
+	"/storage/liuxiaodongLab/liaozizhuo/Projects/repli-ATAC-seq_control/bwa_all_rawdata/LZZ-9-3_S99_sort_rmdup_rmor_q30.bam"
 	"/storage/liuxiaodongLab/liaozizhuo/Projects/repli-ATAC-seq_control/bwa_all_rawdata/LZZ-8-1_S99_sort_rmdup_rmor_q30.bam"
 	"/storage/liuxiaodongLab/liaozizhuo/Projects/repli-ATAC-seq_control/bwa_all_rawdata/LZZ-8-2_S99_sort_rmdup_rmor_q30.bam"
 	"/storage/liuxiaodongLab/liaozizhuo/Projects/repli-ATAC-seq_control/bwa_all_rawdata/LZZ-8-3_S99_sort_rmdup_rmor_q30.bam"
@@ -14,12 +14,18 @@ files=(
 	"/storage/liuxiaodongLab/liaozizhuo/Projects/repli-ATAC-seq/bwa_all_rawdata/LZZ-2-e_S99_sort_rmdup_rmor_q30.bam"
 	"/storage/liuxiaodongLab/liaozizhuo/Projects/repli-ATAC-seq/bwa_all_rawdata/LZZ-1-f_S99_sort_rmdup_rmor_q30.bam"
 	"/storage/liuxiaodongLab/liaozizhuo/Projects/repli-ATAC-seq/bwa_all_rawdata/LZZ-1-d-sup_S99_sort_rmdup_rmor_q30.bam"
+	"/storage/liuxiaodongLab/liaozizhuo/Projects/repli-ATAC-seq/01.RawData/origin-2_origin-2-4_sort_rmdup_rmor_q30.bam"
+	"/storage/liuxiaodongLab/liaozizhuo/Projects/repli-ATAC-seq/01.RawData/origin-2_origin-2-5_sort_rmdup_rmor_q30.bam"
+	"/storage/liuxiaodongLab/liaozizhuo/Projects/repli-ATAC-seq/01.RawData/origin-2_origin-2-6_sort_rmdup_rmor_q30.bam"
+	"/storage/liuxiaodongLab/liaozizhuo/Projects/repli-ATAC-seq/01.RawData/origin-origin-4_sort_rmdup_rmor_q30.bam"
+	"/storage/liuxiaodongLab/liaozizhuo/Projects/repli-ATAC-seq/01.RawData/origin-origin-5_sort_rmdup_rmor_q30.bam"
+	"/storage/liuxiaodongLab/liaozizhuo/Projects/repli-ATAC-seq/01.RawData/origin-origin-6_sort_rmdup_rmor_q30.bam"
 	)
 
 for file in ${files[@]}; do
 	name=$(basename ${file})
 	name=${name%%.*}
-	bedtools intersect -abam ${file} -b merged_peaks_p0.01_sort -wa > ${name}_filtered.bam
+	bedtools intersect -abam ${file} -b merged_peaks.bed -wa > ${name}_filtered.bam
     samtools index ${name}_filtered.bam
 done
 
