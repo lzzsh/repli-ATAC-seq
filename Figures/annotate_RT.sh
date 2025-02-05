0fa5259b27366fd1f831143fe7fe2f7bb6345180
lzzsh <liaozizhuo@westlake.edu.cn>
2025-02-05 17:15:45 +0800

    Update annotate_RT.sh

--- Figures/annotate_RT.sh
+++ Figures/annotate_RT.sh
@@ -2,8 +2,8 @@
 
 # IDR_peaks
 bedtools intersect -a ZH11_RT.gff3 -b annotate.bed -wa -wb > annotate_RT.bed
-cut -f 1,2,3,4,8 annotate_RT.bed > annotate_RT_temp.bed 
-mv annotate_RT_temp.bed annotate_RT.bed
+cut -f 1,2,3,4,8 annotate_RT.bed > annotate_RT_temp.bed
+sort -k2,2n annotate_RT_temp.bed | uniq > annotate_RT.bed
 
 # ZH11-2_peaks
 bedtools intersect -a ZH11_2_RT_control.gff3 -b annotate.bed -wa -wb > annotate_RT_2.bed
