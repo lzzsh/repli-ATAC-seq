ed2f0f7707d29a52a5e61bd8028df748e8c0e88a
lzzsh <liaozizhuo@westlake.edu.cn>
2026-03-29 00:05:58 +0800

    Update compute_matrix.sh

--- repli-seq/compute_matrix.sh
+++ repli-seq/compute_matrix.sh
@@ -25,3 +25,17 @@ computeMatrix reference-point  -b 2000 -a 2000 -R /storage2/liuxiaodongLab/liaoz
 plotProfile -m matrix_all_peaks_TCX2_3.gz  -out TSS_profile_all_peaks_TCX2_3.pdf  --refPointLabel peak --regionsLabel ES MS LS --outFileSortedRegions location_all_peaks_TCX2_3.bed --plotFileFormat pdf --perGroup
 plotHeatmap -m matrix_all_peaks_TCX2_3.gz  -out TSS_heatmap_all_peaks_TCX2_3.pdf  --refPointLabel peak --regionsLabel ES MS LS --outFileSortedRegions location_all_peaks_TCX2_3.bed --plotFileFormat pdf --perGroup \
 --samplesLabel signal_G1 signal_ES signal_MS signal_LS --heatmapHeight 20 --heatmapWidth 5 --colorMap OrRd 
+
+## SOL1-8 CR
+computeMatrix reference-point  -b 2000 -a 2000 -R /storage2/liuxiaodongLab/liaozizhuo/Projects/repli-seq-CR/heatmap/all_ES.bed \
+	/storage2/liuxiaodongLab/liaozizhuo/Projects/repli-seq-CR/heatmap/all_MS.bed \
+	/storage2/liuxiaodongLab/liaozizhuo/Projects/repli-seq-CR/heatmap/all_LS.bed -S \
+	/storage2/liuxiaodongLab/liaozizhuo/Projects/repli-seq-CR/bwa_all_rawdata/bws/LZZ-13-10-sup.bw\
+	/storage2/liuxiaodongLab/liaozizhuo/Projects/repli-seq-CR/bwa_all_rawdata/bws/LZZ-12-10.bw\
+	/storage2/liuxiaodongLab/liaozizhuo/Projects/repli-seq-CR/bwa_all_rawdata/bws/LZZ-14-11.bw\
+	/storage2/liuxiaodongLab/liaozizhuo/Projects/repli-seq-CR/bwa_all_rawdata/bws/LZZ-16-12.bw\
+--skipZeros --missingDataAsZero -o matrix_all_peaks_SOL1_8.gz --outFileSortedRegions regions_all_peaks_SOL1_8.bed --outFileNameMatrix matrix_all_peaks_SOL1_8.tsv.gz
+
+plotProfile -m matrix_all_peaks_SOL1_8.gz  -out TSS_profile_all_peaks_SOL1_8.pdf  --refPointLabel peak --regionsLabel ES MS LS --outFileSortedRegions location_all_peaks_SOL1_8.bed --plotFileFormat pdf --perGroup
+plotHeatmap -m matrix_all_peaks_SOL1_8.gz  -out TSS_heatmap_all_peaks_SOL1_8.pdf  --refPointLabel peak --regionsLabel ES MS LS --outFileSortedRegions location_all_peaks_SOL1_8.bed --plotFileFormat pdf --perGroup \
+--samplesLabel signal_G1 signal_ES signal_MS signal_LS --heatmapHeight 20 --heatmapWidth 5 --colorMap OrRd 
